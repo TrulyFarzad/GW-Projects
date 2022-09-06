6d0adf686494740ef794717607b4ef7163ab8ba0
Farzad Norasteh <80213194+TrulyFarzad@users.noreply.github.com>
2022-09-06 10:43:33 +0430

    Delete info directory

--- info/csv_database.py
+++
@@ -1,3 +0,0 @@
-# this path will be used in the exporter.py file as the path used to read from and write to the CSV database in the
-# update_csv and save_csv functions.
-PATH = r'enter CSV database path here'
--- info/mysql_connection_info.py
+++
@@ -1,6 +0,0 @@
-# used in the main.py file to connect to the MySQL database.
-HOST = 'localhost'
-USER = 'root'
-PASSWORD = 'password'
-PORT = '3031'
-DATABASE = 'nrst_project'
